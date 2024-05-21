0892a61c626585929a9dd3f1f1803c03a3ea1b44
alex <12alexmaxu@gmail.com>
2024-05-21 11:31:00 +0200

    First Commit

---
+++ AFFTheMovieDB/AFFTheMovieDB/AFFTheMovieDBApp.swift
@@ -0,0 +1,17 @@
+//
+//  AFFTheMovieDBApp.swift
+//  AFFTheMovieDB
+//
+//  Created by Alex  on 21/5/24.
+//
+
+import SwiftUI
+
+@main
+struct AFFTheMovieDBApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
